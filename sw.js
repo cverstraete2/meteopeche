135c96971a55619aa2e98bcf3a2520d5b043eb9f
Cedric Verstraete <ced.verstraete@gmail.com>
2026-06-04 22:16:59 +0200

    Remove today hamburger button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meteocatch-shell-20260604-today-app-bg-rosace";
+const CACHE_NAME = "meteocatch-shell-20260604-today-no-hamburger";
 const APP_SHELL = [
   "./",
   "./index.html",
