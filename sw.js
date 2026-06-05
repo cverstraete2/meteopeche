f53c6a24be97b3fac92f58acd730f58af41b97eb
Cedric Verstraete <ced.verstraete@gmail.com>
2026-06-05 16:59:55 +0200

    Simplify today curve label

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meteocatch-shell-20260605-today-curve-caption";
+const CACHE_NAME = "meteocatch-shell-20260605-today-curve-label-clean";
 const APP_SHELL = [
   "./",
   "./index.html",
