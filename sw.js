57bcac78007b148d32f223e7ba2cdaeead6b2be7
Cedric Verstraete <ced.verstraete@gmail.com>
2026-06-05 12:58:43 +0200

    Subtle today depth dial styling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meteocatch-shell-20260605-today-slim-arrow";
+const CACHE_NAME = "meteocatch-shell-20260605-today-subtle-depth-dial";
 const APP_SHELL = [
   "./",
   "./index.html",
