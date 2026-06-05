34f4a44fb71da7c8bb69d294d250edac40bfb413
Cedric Verstraete <ced.verstraete@gmail.com>
2026-06-05 12:33:36 +0200

    Fix today spot menu layering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meteocatch-shell-20260605-today-depth-wheel";
+const CACHE_NAME = "meteocatch-shell-20260605-today-menu-layer-fix";
 const APP_SHELL = [
   "./",
   "./index.html",
